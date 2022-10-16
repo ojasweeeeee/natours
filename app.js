eb05b0a599918c22c60d631120c620afd99603a1
Ojaswi Saxena <ojaswisaxena14@gmail.com>
2022-10-17 00:04:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express=require('express');
+const path=require('path');
+const morgan=require('morgan');
+const { ppid } = require('process');
+const rateLimit=require('express-rate-limit');
+const helmet=require('helmet');
+const mongoSanitize=require('express-mongo-sanitize');
+const xss=require('xss-clean');
+const hpp=require('hpp');
+
+const AppError=require('./utils/appError');
+const globalErrorHandler=require('./controllers/errorController');
+const tourRouter=require('./routes/tourRoutes');
+const userRouter=require('./routes/userRoutes');
+const reviewRouter=require('./routes/reviewRoutes');
+
+const app=express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname,'views'));
+
+//middlewares
+
+app.use(express.static(path.join(__dirname,'public'))); //serve static files
+
+app.use(helmet()) //set security http headers
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+const limiter=rateLimit({
+    max: 100,
+    windowMs: 60*60*1000,
+    message: 'Too many requests from this IP. Please try again in an hour.'
+})
+app.use('/api',limiter);
+
+app.use(express.json({limit: '10kb'}));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(hpp({
+    whitelist: ['duration','ratingsQuantity','ratingsAverage','maxGroupSize','difficulty','price']
+}));
+
+app.use((req,res,next)=>{
+    req.requestTime=new Date().toISOString();
+    next();
+});
+
+
+//routes
+app.get('/', (req,res)=>{
+    res.status(200).render('base',{
+        tour: 'The Forest Hiker',
+        user: 'Ojaswi'
+    });
+})
+app.get('/overview', (req,res)=>{
+    res.status(200).render('overview',{
+        title: 'All Tours'
+    });
+})
+app.get('/tour', (req,res)=>{
+    res.status(200).render('tour',{
+        title: 'The Forest Hiker'
+    });
+})
+app.use('/api/v1/tours',tourRouter);
+app.use('/api/v1/users',userRouter);
+app.use('/api/v1/reviews',reviewRouter);
+
+app.all('*', (req,res,next)=>{
+    next(new AppError(`Cannot find ${req.originalUrl} on this server.`, 404));
+})
+
+app.use(globalErrorHandler);
+
+module.exports=app;
---
+++ utils/email.js
@@ -0,0 +1,23 @@
+const nodemailer=require('nodemailer');
+
+const sendEmailNow=async options=>{
+    const transporter=nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth:{
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    const mailOptions={
+        from: 'Ojaswi Saxena <ojaswisaxena14@gmail.com>',
+        to: options.email,
+        subject: options.subject,
+        text: options.message
+    }
+
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports=sendEmailNow;
